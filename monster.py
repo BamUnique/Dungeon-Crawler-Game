bde0622f9781d4f67740b046b3ce34725de300af
Banjo Kavanagh <tyranids1234@gmail.com>
2022-06-16 14:55:09 +1000

    Positioning Changes
    Moved around the positioning of the monster_index to be in a better position where it changes it until we get the next monster.

--- monster.py
+++ monster.py
@@ -9,35 +9,37 @@ global monster_name, monster_hp, monster_defence, monster_strength, monster_wisd
 class Monster():
 
     def __init__(self, monster_index, dungeonLevel, level):
+        if level < 20:
+            monster_index = 0
+        elif level < 50:
+            monster_index = 1
+        elif level < 100:
+            monster_index = 2
+        elif level < 250:
+            monster_index = 3
+        elif level < 1000:
+            monster_index = 4
+        elif level >= 1000:
+            monster_index = 5
+
         self.dungeonLevel = dungeonLevel
         self.monster_class_index = monster_index
 
+        self.get_monster(monster_index, level)
 
 
-def get_monster(self, monster_index, level):
+    def get_monster(self, monster_index, level):
 
-    if level < 20:
-        monster_index = 0
-    elif level < 50:
-        monster_index = 1
-    elif level < 100:
-        monster_index = 2
-    elif level < 250:
-        monster_index = 3
-    elif level < 1000:
-        monster_index = 4
-    elif level >= 1000:
-        monster_index = 5
-    with open("monster.json", 'r', encoding='utf-8') as f:
-        monster_classes = json.load(f)
+        with open("monster.json", 'r', encoding='utf-8') as f:
+            monster_classes = json.load(f)
 
-    current_monster = monster_classes["monsters"][monster_index]
-    setattr(self, 'monster_class_name', current_monster['name'])
+        current_monster = monster_classes["monsters"][monster_index]
+        setattr(self, 'monster_class_name', current_monster['name'])
 
-    monster_stats = lambda name, points: setattr(self, name, float(points))
+        monster_stats = lambda name, points: setattr(self, name, float(points))
 
-    for skill_name, skill_points in current_monster["stats"].items():
-        monster_stats(skill_name, skill_points)
+        for skill_name, skill_points in current_monster["stats"].items():
+            monster_stats(skill_name, skill_points)
 
 
 def summon_monsters(monster_index, level):
