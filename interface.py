88c0c649945da14bb643606b7cc5a8ae847051ab
Banjo Kavanagh <tyranids1234@gmail.com>
2022-06-20 15:12:25 +1000

    More cleaning + new interface
    Cleaned up the "line too long" errors also I added the in game interface function which will be played after every dungeon level giving the player the option to go back to the main menu.

--- interface.py
+++ interface.py
@@ -9,11 +9,16 @@ import item
 
 def main_menu():
     init(autoreset=True)
-    print(Style.DIM + Fore.LIGHTRED_EX + " ██████  ██████  ███    ███ ██████   █████  ████████" + Style.RESET_ALL + "     ███████ ██ ███    ███ ██    ██ ██       █████  ████████  ██████  ██████")
-    print(Style.DIM + Fore.LIGHTRED_EX + "██      ██    ██ ████  ████ ██   ██ ██   ██    ██" + Style.RESET_ALL + "        ██      ██ ████  ████ ██    ██ ██      ██   ██    ██    ██    ██ ██   ██")
-    print(Style.DIM + Fore.LIGHTRED_EX + "██      ██    ██ ██ ████ ██ ██████  ███████    ██" + Style.RESET_ALL + "        ███████ ██ ██ ████ ██ ██    ██ ██      ███████    ██    ██    ██ ██████")
-    print(Style.DIM + Fore.LIGHTRED_EX + "██      ██    ██ ██  ██  ██ ██   ██ ██   ██    ██" + Style.RESET_ALL + "             ██ ██ ██  ██  ██ ██    ██ ██      ██   ██    ██    ██    ██ ██   ██")
-    print(Style.DIM + Fore.LIGHTRED_EX + " ██████  ██████  ██      ██ ██████  ██   ██    ██" + Style.RESET_ALL + "        ███████ ██ ██      ██  ██████  ███████ ██   ██    ██     ██████  ██   ██")
+    print(Style.DIM + Fore.LIGHTRED_EX + " ██████  ██████  ███    ███ ██████   █████  ████████" + Style.RESET_ALL +
+          "     ███████ ██ ███    ███ ██    ██ ██       █████  ████████  ██████  ██████")
+    print(Style.DIM + Fore.LIGHTRED_EX + "██      ██    ██ ████  ████ ██   ██ ██   ██    ██" + Style.RESET_ALL +
+          "        ██      ██ ████  ████ ██    ██ ██      ██   ██    ██    ██    ██ ██   ██")
+    print(Style.DIM + Fore.LIGHTRED_EX + "██      ██    ██ ██ ████ ██ ██████  ███████    ██" + Style.RESET_ALL +
+          "        ███████ ██ ██ ████ ██ ██    ██ ██      ███████    ██    ██    ██ ██████")
+    print(Style.DIM + Fore.LIGHTRED_EX + "██      ██    ██ ██  ██  ██ ██   ██ ██   ██    ██" + Style.RESET_ALL +
+          "             ██ ██ ██  ██  ██ ██    ██ ██      ██   ██    ██    ██    ██ ██   ██")
+    print(Style.DIM + Fore.LIGHTRED_EX + " ██████  ██████  ██      ██ ██████  ██   ██    ██" + Style.RESET_ALL +
+          "        ███████ ██ ██      ██  ██████  ███████ ██   ██    ██     ██████  ██   ██")
 
 def main_menu_option():
     print("Choose an option:")
@@ -42,19 +47,41 @@ def class_choice(traveller_name):
 
 def start_game_message(class_index, traveller_name, armour_name, weapon_name):
 
-    berserker_message = f"You woke up this morning with a burning fire in the pit of your stomach, finally you are able to equip your worn {armour_name} and \n" \
-                        f"sheath your {weapon_name} in its scabbard on your hip, after this you say farewell to your family, you enter the dungeon.."
+    berserker_message = f"You woke up this morning with a burning fire in the pit of your stomach, finally you are " \
+                        f"able to equip your worn {armour_name} and \n" \
+                        f"sheath your {weapon_name} in its scabbard on your hip, after this you say farewell " \
+                        f"to your family, you enter the dungeon.."
 
-    archer_message = f"Within minutes after getting the message from the King about the dungeon that has claimed many warriors, you have already donned your {armour_name,}\n" \
-                     f" filled your quiver and made sure {weapon_name} was on your horse, then you make haste towards the dungeon.."
+    archer_message = f"Within minutes after getting the message from the King about the dungeon that has claimed many " \
+                     f"warriors, you have already donned your {armour_name,}\n" \
+                     f" filled your quiver and made sure {weapon_name} was on your horse, then " \
+                     f"you make haste towards the dungeon.."
 
-    mage_message = f"Your brain raced as you went to pack for the journey, finally it was time to put the years of studying the arcance to use. You polish your {weapon_name} \n" \
+    mage_message = f"Your brain raced as you went to pack for the journey, finally " \
+                   f"it was time to put the years of studying the arcance to use. You polish your {weapon_name} \n" \
                    f"and slip it into {armour_name}, then on your magic broom, start the long trip to the dungeon.."
 
-    tank_message = f"When faced with the challenge of the dungeon you have your strong {armour_name} equipped alongside your trusty {weapon_name}, slowly you made your way \n" \
+    tank_message = f"When faced with the challenge of the dungeon you have your strong {armour_name} equipped " \
+                   f"alongside your trusty {weapon_name}, slowly you made your way \n" \
                    f"into the dungeon.."
 
     messages = [berserker_message, archer_message, mage_message, tank_message]
 
     class_message = messages[class_index]
     print(class_message)
+
+
+def in_game_interface():
+    x = False
+    while x:
+        print("Choose an option:")
+        print("1. Enter the Dungeon")
+        print("2. Inventory")
+        print("3. Quit Game")
+        option = input()
+        if option == '1':
+            x = True
+        elif option == '2':
+            x = True
+        elif option == '3':
+            x = True
