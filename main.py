1691a8812c8774fb435aa830f19771421577c870
Banjo Kavanagh <tyranids1234@gmail.com>
2022-06-20 22:26:36 +1000

    Equipment Class
    Added the Equipment class from player.py to the main.py file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import dungeon
 import interface
 from player import Player
 from monster import Monster
+from player import Equipment
 
 
 if __name__ == '__main__':
@@ -18,14 +19,15 @@ if __name__ == '__main__':
 
     p = Player(chosen_class, traveller_name, dungeonLevel)
 
-    weapon = (p.inventory[0])["name"]
-    armour = (p.inventory[1])
     cooldown = 0
 
-    interface.start_game_message(chosen_class, traveller_name, armour, weapon)
-    while game_run == True:
+    interface.start_game_message(chosen_class, p)
 
+    game_run = True
 
+    e = Equipment(p.inventory)
+
+    while game_run:
 
         maxLevel, level = dungeon.entering_dungeon(chosen_class, traveller_name, dungeonLevel)
 
@@ -33,8 +35,8 @@ if __name__ == '__main__':
 
             m = Monster(dungeonLevel, level)
 
-            current_abilities = dungeon.dungeon_interaction(chosen_class, class_name, traveller_name,
-                                                                              dungeonLevel, maxLevel, level, m.index)
+            current_abilities = dungeon.dungeon_interaction(chosen_class, traveller_name,
+                                                                              dungeonLevel, maxLevel, level)
 
             attack_first = damage.who_fights_first(m, p)
 
